7d9d705d7bbac3d951bd7b88539158d80871f2f1
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-06 17:00:17 +0100

    cut display to 9 asteroids per page and added navigation for results

--- src/assets/components/DisplayArea/DisplayArea.tsx
+++ src/assets/components/DisplayArea/DisplayArea.tsx
@@ -1,14 +1,66 @@
+import { useState, useEffect } from "react";
 import AsteroidList from "../../containers/AsteroidList/AsteroidList";
 import Navigation from "../Navigation/Navigation";
+import fetchAsteroids from "../../../api";
 
+const itemsPerPage = 9;
 
 function DisplayArea() {
+    const [asteroids, setAsteroids] = useState<any[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    // date range ** change when search bar added **
+    const startDate = "2024-01-01";
+    const endDate = "2024-01-31";
+
+    useEffect(() => {
+        // Fetch asteroids when the component renders
+        fetchAsteroids(startDate, endDate)
+            .then((fetchedAsteroids) => {
+                setAsteroids(fetchedAsteroids);
+            })
+            // If there's an error, update the error state with the error message
+            .catch((error: any) => {
+                setError(error.message);
+            })
+            // Show loading text:
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []); // Empty dependency array means this runs once on component mount
+
+    const totalItems = asteroids.length; // calculate total number of asteroids returned
+
+    // function to handle page changes
+    const handlePageChange = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+    };
+
+    // calculate when to slice asteroid array to get 9 asteroids per page:
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const currentAsteroids = asteroids.slice(
+        startIndex,
+        startIndex + itemsPerPage
+    );
+
+    // Show loading message while data is being fetched
+    if (loading) return <p>Loading...</p>;
+
+    // Show error message if there's an error
+    if (error) return <p>Error: {error}</p>;
+
     return (
         <div>
-            <AsteroidList />
-            <Navigation />
+            <AsteroidList asteroids={currentAsteroids} />
+            <Navigation
+                totalItems={totalItems}
+                itemsPerPage={itemsPerPage}
+                onPageChange={handlePageChange}
+            />
         </div>
     );
-  }
-  
-  export default DisplayArea;
+}
+
+export default DisplayArea;
--- src/assets/containers/AsteroidList/AsteroidList.tsx
+++ src/assets/containers/AsteroidList/AsteroidList.tsx
@@ -1,42 +1,11 @@
 import './AsteroidList.scss';
 import AsteroidCard from "../../components/AsteroidCard/AsteroidCard";
-import fetchAsteroids from "../../../api";
-import { useEffect, useState } from "react";
 
-const AsteroidList = () => {
-    const [asteroids, setAsteroids] = useState<any[]>([]);
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(true); // Add loading state
+type AsteroidListProps = {
+    asteroids: any[];
+};
 
-    const startDate = "2024-01-01";
-    const endDate = "2024-01-31";
-
-    useEffect(() => {
-        const getAsteroids = async () => {
-            try {
-                // Fetch asteroids when the component renders
-                const fetchedAsteroids = await fetchAsteroids(startDate, endDate);
-                console.log("Fetched asteroids:", fetchedAsteroids);
-                setAsteroids(fetchedAsteroids);
-            } catch (error: any) {
-                // If there's an error, update the error state with the error message
-                setError(error.message);
-            } finally {
-                // Update loading state
-                setLoading(false);
-            }
-        };
-
-        getAsteroids();
-    }, []); // Empty dependency array means this runs once on component mount
-
-    // Show loading message while data is being fetched
-    if (loading) return <p>Loading...</p>;
-
-    // Show error message if there's an error
-    if (error) return <p>Error: {error}</p>;
-
-    // Render list of asteroids
+const AsteroidList = ({ asteroids }: AsteroidListProps) => {
     return (
         <div className="asteroid-list">
             {asteroids.length === 0 ? (

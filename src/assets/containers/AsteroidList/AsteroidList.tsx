bfce935c7696f4a52af10d599738c22ca3539dd7
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-06 15:23:34 +0100

    Added api.ts file to fetch the asteroid data needed and got the display working with AsteroidCard.tsx and AsteroidList.tsx

--- src/assets/containers/AsteroidList/AsteroidList.tsx
+++ src/assets/containers/AsteroidList/AsteroidList.tsx
@@ -1,12 +1,44 @@
 import AsteroidCard from "../../components/AsteroidCard/AsteroidCard";
+import fetchAsteroids from "../../../api";
+import { useEffect, useState } from "react";
 
+const AsteroidList = () => {
+    const [asteroids, setAsteroids] = useState<any[]>([]);
+    const [error, setError] = useState<string | null>(null);
 
-function AsteroidList() {
+    const startDate = "2024-01-01";
+    const endDate = "2024-01-31";
+
+    useEffect(() => {
+        // Fetch asteroids when the component renders
+        fetchAsteroids(startDate, endDate)
+            .then((fetchedAsteroids) => {
+                console.log("Fetched asteroids:", fetchedAsteroids);
+                setAsteroids(fetchedAsteroids);
+            })
+            .catch((error: any) => {
+                setError(error.message);
+            });
+    }, []);
+
+    //   if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    // render list of asteroids:
     return (
         <div>
-            <AsteroidCard />
+            {asteroids.length === 0 ? (
+                <p>No asteroids found.</p>
+            ) : (
+                asteroids.map((asteroid: any, index: number) => (
+                    <AsteroidCard
+                        key={`asteroid-${index}`}
+                        asteroid={asteroid}
+                    />
+                ))
+            )}
         </div>
     );
-  }
-  
-  export default AsteroidList;
+};
+
+export default AsteroidList;
